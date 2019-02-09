69f2e1ef4df826d229186c227a2c5893214b0f96
caojianmin <46328885+caojianmin@users.noreply.github.com>
2019-02-09 14:31:55 +0800

    Revert "Initial commit"
    This reverts commit 43fd40d4aca24f7c45ff915c03e8dafbed68d215.

--- ethereum/compaign.js
+++
@@ -1,6 +0,0 @@
-import web3 from './web3';
-import Compaign from './build/Compaign.json';
-
-export default (address)=>{
-  return new web3.eth.Contract(JSON.parse(Compaign.interface),address);
-};
--- ethereum/factory.js
+++
@@ -1,7 +0,0 @@
-import web3 from './web3';
-import CompaignFactory from './build/CompaignFactory.json';
-const instance = new web3.eth.Contract(
-  JSON.parse(CompaignFactory.interface),
-  '0xcBAe3a6D690FB07Fc53697813576508C7EB97F5f'
-);
-export default instance;
